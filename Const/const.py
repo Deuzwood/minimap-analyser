f52c04a10a4820a3723b122430a970944625f5b4
Deuzwood <nicolaswallet@hotmail.fr>
2021-02-12 23:42:25 +0100

    pyLint

--- Const/const.py
+++ Const/const.py
@@ -1,19 +1,29 @@
-#--------------------
+# --------------------
 # Constant file
-#--------------------
+# --------------------
 
 # League of Legends Version
 LATEST_VERSION = "11.2.1"
 
 # CDN
-CDN_DOWNLOAD_LINK = "https://ddragon.leagueoflegends.com/cdn/dragontail-"+LATEST_VERSION+".tgz"
+CDN_DOWNLOAD_LINK = (
+    "https://ddragon.leagueoflegends.com/cdn/dragontail-" + LATEST_VERSION + ".tgz"
+)
 
 # List path from Data Dragon
-CHAMPIONS_LIST_PATH = "dragontail-"+LATEST_VERSION+"/"+LATEST_VERSION+"/data/fr_FR/champion.json"
+CHAMPIONS_LIST_PATH = (
+    "dragontail-" + LATEST_VERSION + "/" + LATEST_VERSION + "/data/fr_FR/champion.json"
+)
 
 # Tile Path
-CHAMPIONS_TILE_PATH = "dragontail-"+LATEST_VERSION+"/"+LATEST_VERSION+"/img/champion/"
+CHAMPIONS_TILE_PATH = (
+    "dragontail-" + LATEST_VERSION + "/" + LATEST_VERSION + "/img/champion/"
+)
 
 
 # red = #de2f2f
 # blue = #1580b6
+
+# Picker from yummi on minimap
+# blue = #3769cc
+# red = #ae302b
