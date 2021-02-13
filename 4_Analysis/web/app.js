cdd213f65599c0d0a887e6463774dcc4ed440e88
Nicolas Wallet <nicolaswallet@hotmail.fr>
2021-02-13 19:36:56 +0100

    Web Interface from https://github.com/Deuzwood/nodejs-mvc/

---
+++ 4_Analysis/web/app.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var path = require('path');
+
+var routes = require('./routes/router');
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.disable('x-powered-by');
+
+app.use('/', routes);
+
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err,
+        error: err.status
+    });
+});
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Server listening on port ' + server.address().port);
+});
