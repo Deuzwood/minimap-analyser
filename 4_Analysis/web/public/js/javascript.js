11c85193cd1c31cd2415995c4aa679432378ac91
Deuzwood <nicolaswallet@hotmail.fr>
2021-04-10 18:23:42 +0200

    Update Interface

--- 4_Analysis/web/public/js/javascript.js
+++ 4_Analysis/web/public/js/javascript.js
@@ -25,11 +25,11 @@ let mapStyle = 'default';
 async function init() {
     try {
         data = await csvLoader(
-            '/data/' + fileToLoadName + '-simple.csv' ||
+            '/data/' + fileToLoadName + '.csv' ||
                 '/data/Detection_Video_Results_full.csv'
         );
 
-        stats = await fetch('/data/' + fileToLoadName + '-simple-stats.json');
+        stats = await fetch('/data/' + fileToLoadName + '-stats.json');
         stats = await stats.json();
 
         const canvas = document.querySelector('#rift');
@@ -91,12 +91,13 @@ function renderGradient(name) {
 }
 
 async function displayStats() {
-    const championsLineStatsCode = (name) => `<tr>
-    <td width="10%"><img src="/data/tiles/${name}.png">${name}</td>
+    const championsLineStatsCode = (stats) => `<tr>
+    <td width="10%"><img src="/data/tiles/${stats.name}.png"></td>
+    <td width="10%">${stats.mainPosition}</td>
     <td>
-        <button class="btn btn-outline-secondary btn-small" data-action="btnPath" data-name="${name}"><i class="bi bi-geo-alt-fill"></i></button>
-        <button class="btn btn-outline-success btn-small" data-action="btnCompare" data-name="${name}"><i class="bi bi-plus"></i></button>
-        <button class="btn btn-outline-warning btn-small" data-action="btnHeatMap" data-name="${name}"><i class="bi bi-thermometer-high"></i></button>
+        <button class="btn btn-outline-secondary btn-small" data-action="btnPath" data-name="${stats.name}"><i class="bi bi-geo-alt-fill"></i></button>
+        <button class="btn btn-outline-success btn-small" data-action="btnCompare" data-name="${stats.name}"><i class="bi bi-plus"></i></button>
+        <button class="btn btn-outline-warning btn-small" data-action="btnHeatMap" data-name="${stats.name}"><i class="bi bi-thermometer-high"></i></button>
     </td>
   </tr>`;
 
@@ -104,10 +105,11 @@ async function displayStats() {
     tbred.innerHTML = '';
 
     for (const property in stats) {
+        console.log(stats[property]);
         if (stats[property].team == 0) {
-            tbblue.innerHTML += championsLineStatsCode(stats[property].name);
+            tbblue.innerHTML += championsLineStatsCode(stats[property]);
         } else {
-            tbred.innerHTML += championsLineStatsCode(stats[property].name);
+            tbred.innerHTML += championsLineStatsCode(stats[property]);
         }
     }
 
@@ -144,7 +146,7 @@ async function renderNew() {
     var img = new Image();
     await new Promise(
         (r) => (img.onload = r),
-        (img.src = '/data/minimap/' + mapStyleName[mapStyle] + '.png')
+        (img.src = '/data/minimap/rift.png')
     );
     ctx.drawImage(img, 0, 0, 256, 256);
 }
@@ -191,103 +193,6 @@ async function renderPath(champ) {
 }
 
 init();
-
-var canvas = document.getElementById('gradient');
-var context = canvas.getContext('2d');
-
-var width = 256; //canvas.width;
-var height = 256; //canvas.height;
-
-var grid = [];
-function init_grid() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    for (var i = 0; i < width; i++) {
-        grid[i] = [];
-        for (var j = 0; j < height; j++) {
-            grid[i][j] = 0.0;
-        }
-    }
-}
-
-var radius = 5;
-
-// from http://homepages.inf.ed.ac.uk/rbf/HIPR2/gsmooth.htm
-function _gaussian(x, y) {
-    stdev = 1;
-    return (
-        (1.0 / Math.sqrt(2 * Math.PI * stdev)) *
-        Math.exp(-(x * x + y * y) / (2 * stdev * stdev))
-    );
-}
-
-// from http://homepages.inf.ed.ac.uk/rbf/HIPR2/gsmooth.htm
-function gaussian_grid(radius) {
-    var _gaussian_grid = [];
-    for (var x = -radius; x <= radius; x++) {
-        _gaussian_grid[x + radius] = [];
-        for (var y = -radius; y <= radius; y++) {
-            _gaussian_grid[x + radius][y + radius] = Math.round(
-                127 * _gaussian(x, y)
-            );
-        }
-    }
-    return _gaussian_grid;
-}
-
-var radius = 3;
-var _gaussian_grid = gaussian_grid(radius);
-
-function gaussian(xi, yi) {
-    return _gaussian_grid[xi + radius][yi + radius];
-}
-
-var maximum = 0;
-function apply_gaussian(x, y) {
-    for (var xi = -radius; xi <= radius; xi++) {
-        for (var yi = -radius; yi <= radius; yi++) {
-            var cell_value = (grid[x + xi][y + yi] += gaussian(xi, yi));
-            maximum = Math.max(cell_value, maximum);
-        }
-    }
-}
-
-function add_point(x, y) {
-    apply_gaussian(x, y);
-}
-
-function each_row(action) {
-    for (var y = 1; y <= height; y++) {
-        action(y);
-    }
-}
-
-function each_cell(action) {
-    each_row(function (y) {
-        for (var x = 1; x <= width; x++) {
-            action(x, y);
-        }
-    });
-}
-
-function print_grid() {
-    each_row(function (y) {
-        console.log(grid[y].join('\t') + '\n');
-    });
-}
-// Render grid translates our grid to the canvas. It ignores all points that have no value (=0)
-// and gives a color to those who have, but this is depending on the maximum value
-// TODO fix the color generation, it does not give a very smooth image
-function render_grid() {
-    each_cell(function (x, y) {
-        var value = grid[x - 1][y - 1];
-        // console.log(value);
-        if (value > 0) {
-            color = (value / maximum) * 255;
-            context.fillStyle = 'rgba(200,' + color + ', 0, 0.5)';
-            context.fillRect(x, y, 1, 1);
-        }
-    });
-}
 
 /* range */
 
