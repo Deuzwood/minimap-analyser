fd00803f56632176cc34d1da3fffa7ff9e2a1988
Nicolas Wallet <nicolaswallet@hotmail.fr>
2021-06-05 12:05:12 +0200

    rm outdated Data + Fix typo

--- Data/Evaluation/convertDataTrainToCsv.py
+++ Data/Evaluation/convertDataTrainToCsv.py
@@ -1,12 +1,12 @@
 csv_file = open("data_train_deepLeague4096.csv", "w")
-txt_file = open("data_train_deepLeague4096.txt","r")
-csv_file.write("image,xmin,xmax,ymin,ymax,label\n")
+txt_file = open("data_train_deepLeague4096.txt", "r")
+csv_file.write("image,xmin,ymin,xmax,ymax,label\n")
 
 line = txt_file.readline()
-while line :
-    line = line.replace("\n","").split(" ")
+while line:
+    line = line.replace("\n", "").split(" ")
     image = line.pop(0)
-    for object in line :
+    for object in line:
         csv_file.write(image+","+object+"\n")
     line = txt_file.readline()
 
